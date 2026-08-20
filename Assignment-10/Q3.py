865fa7abbeb103ffc450b13b52bbe527945cd45c
Bhakti-coder <bhhaktikabule04@gmail.com>
2026-08-20 23:27:37 +0530

    Add Assignment 10 -  alternative solutions for digit problems

--- Assignment-10/Q3.py
+++ Assignment-10/Q3.py
@@ -11,3 +11,21 @@ while num > 0:
         count += 1
     num = num // 10
 print("Count of digits between 3 and 7:", count)
+
+
+# Using function
+
+def count_digits(n):
+    count = 0
+    while n > 0:
+        digit = n % 10
+
+        if 3 <= digit <= 7:
+            count += 1
+
+        n //= 10
+    return count
+
+
+n = int(input("Enter number: "))
+print("Count:", count_digits(n))
